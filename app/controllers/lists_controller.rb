84caf382feb498581495dd715fd6b6ccac840841
Juliana Arrighi <jarrighi@poli.ufrj.br>
2022-02-24 15:20:26 -0300

    list controller and view backend done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,21 @@
 class ListsController < ApplicationController
   def index
-    lists = List.all
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path(@lists)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :movies)
   end
 end
