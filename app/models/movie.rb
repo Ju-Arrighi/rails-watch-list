aedcfa349e9d38c31b7aa0f4a629f8139b61dcd6
Juliana Arrighi <jarrighi@poli.ufrj.br>
2022-02-24 14:06:58 -0300

    routes done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,4 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, :overview, presence: :true
   validates :title, uniqueness: true
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  resources :lists, only:[:index, :create, :show] do
+  resources :lists, only:[:index, :new, :create, :show] do
     resources :reviews, only: :create
     resources :bookmarks, only:[:new, :create]
   end
